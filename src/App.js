51ef0c869d351351a279da8e584b24530a1fc4a0
learntechpuzz <learntechpuzz@gmail.com>
2019-06-11 14:52:46 +0530

    adding logo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Routes from './Routes';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Header from './modules/header/components/Header';
 import Footer from './modules/footer/components/Footer';
+import { Container } from 'react-bootstrap';
 
 const App = () => (
-      <BrowserRouter>
-        <Header />
-        <Routes />
-        <Footer />
-      </BrowserRouter>
+  <BrowserRouter>
+    <Container>       
+      <Header />
+      <Routes />
+      <Footer />
+    </Container>
+
+  </BrowserRouter>
 );
 
 export default App;
--- src/modules/header/components/Header.js
+++ src/modules/header/components/Header.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import logo from './../../../logo.png'
 
 const Header = () => {
     return (
-        <div>This is header</div>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#home">
+                    <img
+                        alt=""
+                        src={logo}
+                        height="30"
+                        className="d-inline-block align-top"
+                    />
+                </Navbar.Brand>
+
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Feedback</Nav.Link>
+                    </Nav>
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Navbar>
     );
 }
 
