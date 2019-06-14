e975a233e26fed19727f1395e6f176315497a3a3
learntechpuzz <learntechpuzz@gmail.com>
2019-06-14 11:41:01 +0530

    favicon

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,8 @@ module.exports = {
   plugins: [
     new HtmlWebPackPlugin({
       template: "./public/index.html",
-      filename: "./index.html"
+      filename: "./index.html",
+      favicon: "./public/favicon.png"
     })
   ]
 };
